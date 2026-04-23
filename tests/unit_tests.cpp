29be2ed0be2d3989227c3fa92bdf8f160cba8237
csv610 <csv610@yahoo.com>
2026-04-23 18:10:02 +0530

    test: expand unit tests to 13 cases with smart pointers
    - Added boundary tests: out of bounds, origin, empty dataset, single point
    - Added k-NN, priority search, epsilon search, fixed-radius tests
    - Added dimension accessor and brute force tests
    - Replaced raw new/delete with std::unique_ptr for RAII

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <ANN/ANN.h>
+#include <memory>
 
 class ANNTest : public ::testing::Test {
 protected:
@@ -11,7 +12,6 @@ protected:
         nn_idx = new ANNidx[1];
         dists = new ANNdist[1];
 
-        // Fill with simple data points: (0,0), (1,1), ..., (9,9)
         for (int i = 0; i < max_pts; i++) {
             data_pts[i][0] = (ANNcoord)i;
             data_pts[i][1] = (ANNcoord)i;
@@ -23,7 +23,7 @@ protected:
         annDeallocPt(query_pt);
         delete[] nn_idx;
         delete[] dists;
-        annClose(); // Clean up internal ANN memory
+        annClose();
     }
 
     int dim;
@@ -35,24 +35,20 @@ protected:
 };
 
 TEST_F(ANNTest, KdTreeBasicSearch) {
-    ANNkd_tree* kdTree = new ANNkd_tree(data_pts, max_pts, dim);
+    std::unique_ptr<ANNkd_tree> kdTree(new ANNkd_tree(data_pts, max_pts, dim));
 
-    // Query point near (5,5)
     query_pt[0] = 5.1;
     query_pt[1] = 5.1;
 
     kdTree->annkSearch(query_pt, 1, nn_idx, dists);
 
     EXPECT_EQ(nn_idx[0], 5);
-    EXPECT_NEAR(dists[0], 0.02, 1e-6); // (5.1-5)^2 + (5.1-5)^2 = 0.01 + 0.01 = 0.02
-
-    delete kdTree;
+    EXPECT_NEAR(dists[0], 0.02, 1e-6);
 }
 
 TEST_F(ANNTest, BdTreeBasicSearch) {
-    ANNbd_tree* bdTree = new ANNbd_tree(data_pts, max_pts, dim);
+    std::unique_ptr<ANNbd_tree> bdTree(new ANNbd_tree(data_pts, max_pts, dim));
 
-    // Query point near (2,2)
     query_pt[0] = 1.9;
     query_pt[1] = 1.9;
 
@@ -60,14 +56,11 @@ TEST_F(ANNTest, BdTreeBasicSearch) {
 
     EXPECT_EQ(nn_idx[0], 2);
     EXPECT_NEAR(dists[0], 0.02, 1e-6);
-
-    delete bdTree;
 }
 
 TEST_F(ANNTest, ExactMatch) {
-    ANNkd_tree* kdTree = new ANNkd_tree(data_pts, max_pts, dim);
+    std::unique_ptr<ANNkd_tree> kdTree(new ANNkd_tree(data_pts, max_pts, dim));
 
-    // Query point exactly at (7,7)
     query_pt[0] = 7.0;
     query_pt[1] = 7.0;
 
@@ -75,8 +68,148 @@ TEST_F(ANNTest, ExactMatch) {
 
     EXPECT_EQ(nn_idx[0], 7);
     EXPECT_EQ(dists[0], 0.0);
+}
 
-    delete kdTree;
+TEST_F(ANNTest, KdTreeQueryOutOfBounds) {
+    std::unique_ptr<ANNkd_tree> kdTree(new ANNkd_tree(data_pts, max_pts, dim));
+
+    query_pt[0] = 100.0;
+    query_pt[1] = 100.0;
+
+    kdTree->annkSearch(query_pt, 1, nn_idx, dists);
+
+    EXPECT_EQ(nn_idx[0], 9);
+    EXPECT_GE(dists[0], 0.0);
+}
+
+TEST_F(ANNTest, KdTreeQueryAtOrigin) {
+    std::unique_ptr<ANNkd_tree> kdTree(new ANNkd_tree(data_pts, max_pts, dim));
+
+    query_pt[0] = 0.0;
+    query_pt[1] = 0.0;
+
+    kdTree->annkSearch(query_pt, 1, nn_idx, dists);
+
+    EXPECT_EQ(nn_idx[0], 0);
+    EXPECT_EQ(dists[0], 0.0);
+}
+
+TEST_F(ANNTest, KdTreeKNearestNeighbors) {
+    std::unique_ptr<ANNkd_tree> kdTree(new ANNkd_tree(data_pts, max_pts, dim));
+
+    query_pt[0] = 5.5;
+    query_pt[1] = 5.5;
+
+    delete[] nn_idx;
+    delete[] dists;
+    nn_idx = new ANNidx[max_pts];
+    dists = new ANNdist[max_pts];
+
+    kdTree->annkSearch(query_pt, max_pts, nn_idx, dists);
+
+    EXPECT_GE(dists[0], 0.0);
+    for (int i = 1; i < max_pts; i++) {
+        EXPECT_GE(dists[i], dists[i-1]);
+    }
+
+    delete[] nn_idx;
+    delete[] dists;
+    nn_idx = nullptr;
+    dists = nullptr;
+}
+
+TEST_F(ANNTest, KdTreePrioritySearch) {
+    std::unique_ptr<ANNkd_tree> kdTree(new ANNkd_tree(data_pts, max_pts, dim));
+
+    query_pt[0] = 5.1;
+    query_pt[1] = 5.1;
+
+    nn_idx = new ANNidx[1];
+    dists = new ANNdist[1];
+
+    kdTree->annkPriSearch(query_pt, 1, nn_idx, dists);
+
+    EXPECT_EQ(nn_idx[0], 5);
+    EXPECT_NEAR(dists[0], 0.02, 1e-6);
+}
+
+TEST_F(ANNTest, KdTreeEpsilonSearch) {
+    std::unique_ptr<ANNkd_tree> kdTree(new ANNkd_tree(data_pts, max_pts, dim));
+
+    query_pt[0] = 5.1;
+    query_pt[1] = 5.1;
+
+    double eps = 0.5;
+
+    kdTree->annkSearch(query_pt, 1, nn_idx, dists, eps);
+
+    EXPECT_GE(nn_idx[0], 0);
+    EXPECT_LT(nn_idx[0], max_pts);
+}
+
+TEST_F(ANNTest, KdTreeEmptyDataset) {
+    ANNpointArray empty_pts = annAllocPts(0, dim);
+    std::unique_ptr<ANNkd_tree> kdTree(new ANNkd_tree(empty_pts, 0, dim));
+
+    EXPECT_EQ(kdTree->nPoints(), 0);
+
+    annDeallocPts(empty_pts);
+}
+
+TEST_F(ANNTest, KdTreeSinglePoint) {
+    ANNpointArray single_pt = annAllocPts(1, dim);
+    single_pt[0][0] = 5.0;
+    single_pt[0][1] = 5.0;
+
+    std::unique_ptr<ANNkd_tree> kdTree(new ANNkd_tree(single_pt, 1, dim));
+
+    query_pt[0] = 5.0;
+    query_pt[1] = 5.0;
+
+    kdTree->annkSearch(query_pt, 1, nn_idx, dists);
+
+    EXPECT_EQ(nn_idx[0], 0);
+    EXPECT_EQ(dists[0], 0.0);
+
+    annDeallocPts(single_pt);
+}
+
+TEST_F(ANNTest, KdTreeDimensionAccessor) {
+    std::unique_ptr<ANNkd_tree> kdTree(new ANNkd_tree(data_pts, max_pts, dim));
+
+    EXPECT_EQ(kdTree->theDim(), dim);
+    EXPECT_EQ(kdTree->nPoints(), max_pts);
+}
+
+TEST_F(ANNTest, BruteForceSearch) {
+    std::unique_ptr<ANNbruteForce> brute(new ANNbruteForce(data_pts, max_pts, dim));
+
+    query_pt[0] = 5.1;
+    query_pt[1] = 5.1;
+
+    brute->annkSearch(query_pt, 1, nn_idx, dists);
+
+    EXPECT_EQ(nn_idx[0], 5);
+    EXPECT_NEAR(dists[0], 0.02, 1e-6);
+}
+
+TEST_F(ANNTest, FixedRadiusSearch) {
+    std::unique_ptr<ANNkd_tree> kdTree(new ANNkd_tree(data_pts, max_pts, dim));
+
+    query_pt[0] = 5.0;
+    query_pt[1] = 5.0;
+
+    ANNdist sqRad = 2.0;
+
+    nn_idx = new ANNidx[max_pts];
+    dists = new ANNdist[max_pts];
+
+    int cnt = kdTree->annkFRSearch(query_pt, sqRad, max_pts, nn_idx, dists);
+
+    EXPECT_GE(cnt, 0);
+    for (int i = 0; i < cnt; i++) {
+        EXPECT_LE(dists[i], sqRad);
+    }
 }
 
 int main(int argc, char **argv) {
